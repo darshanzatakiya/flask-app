525389770d05af3916046f7687f05987676a4129
Haridugrani <bhargavdungrani32@gmail.com>
2025-10-31 14:16:22 +0530

    db connection

--- app.py
+++ app.py
@@ -4,13 +4,15 @@ import pymysql
 app = Flask(__name__)
 
 def get_db_connection():
-    connection = pymysql.connect(host='mydb.c5ayy6koa5gm.eu-central-1.rds.amazonaws.com',  # Replace with your RDS endpoint
-                                 user='dbuser',      # Replace with your RDS username
-                                 password='dbpassword',  # Replace with your RDS password
-                                 db='devprojdb',   # Replace with your database name
-                                 charset='utf8mb4',
-                                 cursorclass=pymysql.cursors.DictCursor)
-    return connection
+    return pymysql.connect(
+        host='mydb.c5ayy6koa5gm.eu-central-1.rds.amazonaws.com',
+        user='dbuser',
+        password='dbpassword',
+        database='devprojdb',
+        charset='utf8mb4',
+        cursorclass=pymysql.cursors.DictCursor
+    )
+
 
 @app.route('/health')
 def health():
